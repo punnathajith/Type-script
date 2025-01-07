ca35d3348b82b33aee0db8afda3745e8c55592f5
Ajithpunnath <Ajithpunnath2000@gmail.com>
2025-01-07 15:28:49 +0530

    Type basics

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,23 @@
-console.log('hello');
-var user = { name: "ajith", age: 25 };
-console.log(user.name);
+//Type basics
+var character = 'ajith';
+var num = 75;
+var isNum = false;
+// character = 75; Type 'number' is not assignable to type 'string'
+//but below is possible
+character = 'abhiraj';
+//num = 'fan'; Type 'string' is not assignable to type 'number'
+//but below is possible
+num = 85;
+//isNum = 'yes'; Type 'string' is not assignable to type 'boolean'
+//but below is possible
+isNum = true;
+//we can't change the type of the variable it will restrict that operation
+// const multibyten = (num)=>{
+//    return num*10;
+// }
+// console.log(multibyten('hello'));  //this way it cause to error but js will not show that we can prevent that by
+var multibyten = function (num) {
+    return num * 10;
+};
+console.log(multibyten(75));
+//we explicitly giving the type of the parameter and avoid the error
